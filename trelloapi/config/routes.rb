db84895ca78b2dd3ed4271ffa7a02ca4d8270044
Mohamed Aly <mohamed.aly06@gmail.com>
2017-09-17 19:20:01 -0400

    Add route to create cards and Rails-friendly link paths

--- trelloapi/config/routes.rb
+++ trelloapi/config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'static_pages/home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
+  get '/help',  to: 'static_pages#help'
+  get '/new',   to: 'cards#new'
   resources :cards
 
 
