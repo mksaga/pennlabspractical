fc47b5a5be998aecb0d5654efd9dba2f653f9253
Mohamed Aly <mohamed.aly06@gmail.com>
2017-09-17 18:40:02 -0400

    Add validations and tests for Card model, create List model

--- trelloapi/app/models/card.rb
+++ trelloapi/app/models/card.rb
@@ -1,2 +1,8 @@
 class Card < ApplicationRecord
+
+  # testing that title and description are present
+  
+  validates :title, presence: true
+  validates :description, presence: true
+
 end
--- trelloapi/test/models/card_test.rb
+++ trelloapi/test/models/card_test.rb
@@ -4,4 +4,23 @@ class CardTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+    @card = Card.new(title: "Dat Card Tho", description: "A card is what this is; a description is where you're at")
+  end
+
+  test "card should be valid" do
+    assert @card.valid?
+  end
+
+  test "title should be present" do
+    @card.title = "    "
+    assert_not @card.valid?
+  end
+
+  test "descr should be present" do
+    @card.description = "      "
+    assert_not @card.valid?
+  end
+
 end
